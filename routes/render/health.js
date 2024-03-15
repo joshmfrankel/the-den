482510655980374fdc154736b1d217568bd39b77
Josh Frankel <joshmfrankel@gmail.com>
2024-03-14 20:29:35 -0400

    Ensure automatic deploy

--- routes/render/health.js
+++ routes/render/health.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-// @todo Add db check or other app sanity check
+// @todo Verify database or other sanity check
 router.get('/', (request, response) => {
   response.sendStatus(200)
 })
